9175b93c44a7bd7a7f7f438d12ca591d83458b5a
Zurglots <bfeole@gmail.com>
2019-10-07 19:55:30 -0400

    Bubble sort completed updated notes

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -21,11 +21,13 @@ def selection_sort(arr):
     return arr
 
 
-# Notepad testing
+# Notes / testing
 
     # if arr[smallest_index] < arr[i]:
     # smallest_index = i
     # arr.insert(smallest_index, arr[i])
+    # The second for loops allows to search the elements to the right of the current index.
+    # Without this, I don't know if its possible but this also makes it very costly.
 
 # arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
 # arr2 = []
@@ -36,11 +38,25 @@ def selection_sort(arr):
 
 
 # # TO-DO:  implement the Bubble Sort function below
+"""
+1. Loop through your array
+    - Compare each element to its neighbor
+    - If elements in wrong position (relative to each other, swap them)
+2. If no swaps performed, stop. Else, go back to the element at index 0 and repeat step 1.
+"""
 
 
-# def bubble_sort(arr):
-
-#     return arr
+def bubble_sort(arr):
+    # loop through array -1 items times
+    for b in range(0, len(arr) - 1):
+        # loop through remainder of array - current index ignore last element since it should be in place if rest passing
+        for u in range(0, len(arr) - 1 - b):
+            # compare neighbor indexes if current index [u] is > than current index + 1 SWAP
+            if arr[u] > arr[u+1]:
+                arr[u], arr[u+1] = arr[u+1], arr[u]
+                # else:
+                # break
+    return arr
 
 
 # # STRETCH: implement the Count Sort function below
